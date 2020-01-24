704561ed8496e1294a4340bceb22ee54996028c0
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2020-01-24 09:47:52 +0100

    Add files via upload

---
+++ C++/punteros.cpp
@@ -0,0 +1,14 @@
+//punteros 2.0 
+
+int x; //entero
+char c;
+float x;
+bool x; //true or false 
+en int x[5] se guarda el numero d la posicion de memoria
+si pongo printf("%x",x)=pones elo numero de la memoria en el que esta almacenado
+char *palabras[4]; /*(contiene 4 direcciones como por ejemplo 11AB01*/
+ejemplo
+V esta en 44ad23 seria *(*palabra)
+* es para meterse dentro de la direccion
+para sacar la s es *(*palabra+4)
+i de silencio = *(*(palabra+3)+6)
