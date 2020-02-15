58f76ff13ee182ad8e40dd0d5644978557434619
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2020-02-15 11:09:22 +0100

    Add files via upload

---
+++ C++/punteros_par.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int numeros[10], *dir_numeros;
+	
+	for(int i=0;i<10;i++){
+		cout<<"Digite un numero["<<i<<"]: ";
+		cin>>numeros[i];
+	}
+	
+	dir_numeros=numeros;//Posicion de memoria comienza numeros
+	
+	for(int i=0;i<10;i++){
+		if(*dir_numeros%2==0){
+			cout<<"El numero "<<*dir_numeros<<" es par"<<endl;
+			cout<<"posicion: "<<dir_numeros<<endl;
+		}
+		dir_numeros++;
+	}
+	getch();
+	return 0;
+}
